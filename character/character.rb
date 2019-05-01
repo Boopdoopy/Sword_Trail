fe4678afdff7348f3c2feba7b8047ca6d354108f
rhonadin <rhonadin@gmail.com>
2019-04-30 20:18:05 -0400

    Write Character Sheet works

--- character/character.rb
+++ character/character.rb
@@ -25,21 +25,14 @@ class Character
   #Introducing Entropy
   def timeCount
     @ailments.each_key do |key|
-<<<<<<< HEAD
     timeLeft = @ailments[key]
     @ailments[key] = timeLeft - 1
-=======
       timeLeft = @ailments[key]
       @ailments[key] = timeLeft - 1
     end
   
   def writeCharacterSheet
-<<<<<<< HEAD
     File.write("./#{@name}.txt", "#{@name}\n#{@ailments}\n")
-=======
-    File.write("./#{@name}.txt", "#{@name}\n#{@ailments}\n"
->>>>>>> 9e2ab9d051c8dcc508d6363fb431f136e8dac2b5
->>>>>>> 77d99bcfcb87c136499d00d65ef28db4acf0a5f2
   end
 
  end
@@ -50,7 +43,6 @@ j = Character.new('Jefe')
 #j.display
 j.applyDisease("Consumption", 4)
 j.timeCount()
-newHash = j.instance_variable_get(:@ailments)
-puts newHash
+
 
 end
