6e418f010d6086d2891aa301d7047838b9d31aa0
Dorian Wood <rhonadin@gmail.com>
2019-05-03 19:18:24 -0400

    Half implemented read function

--- character/character.rb
+++ character/character.rb
@@ -27,22 +27,28 @@ class Character
     @ailments.each_key do |key|
     timeLeft = @ailments[key]
     @ailments[key] = timeLeft - 1
-      timeLeft = @ailments[key]
-      @ailments[key] = timeLeft - 1
     end
+  end
   
   def writeCharacterSheet
     File.write("./#{@name}.txt", "#{@name}\n#{@ailments}\n")
   end
 
+
+  def printCharacter
+    puts "#{@name}\n#{@ailments}\n"
+  end
  end
 
  #ailments = { "Consumption" => 1, "Space Aids" => 2 }
 
-j = Character.new('Jefe')
-#j.display
-j.applyDisease("Consumption", 4)
-j.timeCount()
+  def readCharacterSheet(char)
+    File.open(char + ".txt", "r") do |f|
+      char = Character.new(f.gets)
+      
+      @ailments = f.gets
+    end
+  end
+j.readCharacterSheet()
+j.printCharacter()
 
-
-end
